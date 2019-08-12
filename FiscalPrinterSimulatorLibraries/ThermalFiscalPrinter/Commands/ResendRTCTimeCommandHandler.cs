925769fa5c3e2fcb036a5f0ef2bb8c7083097391
Wojcik Michal <m.mwojcik@outlook.com>
2019-08-12 20:26:42 +0200

    [Thermal] ReSend RTC Time CommandHandler.

---
+++ FiscalPrinterSimulatorLibraries/ThermalFiscalPrinter/Commands/ResendRTCTimeCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using FiscalPrinterSimulatorLibraries.Commands;
+using FiscalPrinterSimulatorLibraries.Models;
+using ThermalFiscalPrinterSimulatorLibraries.Models;
+
+namespace ThermalFiscalPrinterSimulatorLibraries.Commands
+{
+    /// <summary>
+    /// Command handler for command LBSENDCK
+    /// </summary>
+    public class ResendRTCTimeCommandHandler : BaseCommandHandler
+    {
+        public ResendRTCTimeCommandHandler(BaseFiscalPrinterCommand command) : base(command)
+        {
+        }
+
+        public override CommandHandlerResponse Handle(IFiscalPrinterState fiscalPrinterState)
+        {
+            var state = fiscalPrinterState as FiscalPrinterState;
+
+            if (state.TimeDiffrenceInMinutes == int.MinValue)
+            {
+                throw new FiscalPrinterSimulatorLibraries.Exceptions.FP_IllegalOperationException("RTC not initialized.");
+            }
+            var actualRTCTime = DateTime.Now.AddMinutes(state.TimeDiffrenceInMinutes);
+            var arrayOfRTCParameters = new List<int> {
+                actualRTCTime.Year,
+                actualRTCTime.Month,
+                actualRTCTime.Day,
+                actualRTCTime.Hour,
+                actualRTCTime.Minute,
+                0
+            };
+
+            var commandParameters = string.Join(';', arrayOfRTCParameters);
+            var outputCommand = new ThermalFiscalPrinterCommand(new string[] { "1" }, "#C", commandParameters);
+
+            return new CommandHandlerResponse(outputCommand);
+        }
+    }
+}
